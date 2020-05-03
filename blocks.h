18d9f361d43a4294d8513b3b3d62af6c3b132be4
RoboGypsy <uzairqamar1998@gmail.com>
2020-05-04 00:23:28 +0500

    Moved scripts to ~/.local/bin/

--- blocks.h
+++ blocks.h
@@ -2,15 +2,15 @@
 static const Block blocks[] = {
 	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{"$HOME/Scripts/dwmblocks/cmus.sh",              1,		20},
+	{"$HOME/.local/bin/dwmblocks/cmus.sh",              1,		20},
 
-	{"$HOME/Scripts/dwmblocks/network.sh",	        10,		0},
+	{"$HOME/.local/bin/dwmblocks/network.sh",	        10,		0},
 	
-	{"$HOME/Scripts/dwmblocks/battery.sh",		5,		0},
+	{"$HOME/.local/bin/dwmblocks/battery.sh",		5,		0},
 
-	{"$HOME/Scripts/dwmblocks/volume.sh",		1,		10},
+	{"$HOME/.local/bin/dwmblocks/volume.sh",		1,		10},
 
-	{"$HOME/Scripts/dwmblocks/time.sh",	        60,		0},
+	{"$HOME/.local/bin/dwmblocks/time.sh",	        60,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
