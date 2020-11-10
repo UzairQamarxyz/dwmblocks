9aa44488796044fa7af0407a40695e0e8e6eeca2
UzayrQamarxyz <uzayrqamarxyz@tutanota.com>
2020-11-11 02:08:22 +0500

    added Date module

--- blocks.def.h
+++ blocks.def.h
@@ -1,20 +1,19 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"cat ~/.pacupdate | sed /📦0/d",					0,		9},
+
+	{"$HOME/.local/bin/dwmblocks/cmus.sh",              1,		20},
+
+	{"$HOME/.local/bin/dwmblocks/network.sh",	        10,		0},
 	
-	{"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"$HOME/.local/bin/dwmblocks/battery.sh",		5,		0},
 
-	{"~/bin/statusbar/volume",						0,		10},
+	{"$HOME/.local/bin/dwmblocks/volume.sh",		1,		10},
 
-	{"xbacklight | sed 's/\\..*//'",					0,		11},
-	
-	{"~/bin/statusbar/battery",						5,		0},
+	{"$HOME/.local/bin/dwmblocks/time.sh",	        1,		0},
 
-	{"sensors | awk '/^temp1:/{print $2}'",				5,		0},
-
-	{"~/bin/statusbar/clock",						5,		0},
+	{"$HOME/.local/bin/dwmblocks/date.sh",	        60,		0},
 };
 
-//sets delimeter between status commands. NULL pointer means no delimeter.
-static const char * delim = " | ";
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static const char * delim = "  ";
--- blocks.h
+++ blocks.h
@@ -1,17 +1,1 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
-static const Block blocks[] = {
-	/*Command*/		/*Update Interval*/	/*Update Signal*/
-
-	{"$HOME/.local/bin/dwmblocks/cmus.sh",              1,		20},
-
-	{"$HOME/.local/bin/dwmblocks/network.sh",	        10,		0},
-	
-	{"$HOME/.local/bin/dwmblocks/battery.sh",		5,		0},
-
-	{"$HOME/.local/bin/dwmblocks/volume.sh",		1,		10},
-
-	{"$HOME/.local/bin/dwmblocks/time.sh",	        30,		0},
-};
-
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static const char * delim = "  ";
+blocks.def.h
