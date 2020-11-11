4cf0d242cfb5655ec846996c1c464815618d211b
UzayrQamarxyz <uzayrqamarxyz@tutanota.com>
2020-11-11 13:33:10 +0500

    updated update intervals

--- blocks.def.h
+++ blocks.def.h
@@ -4,15 +4,15 @@ static const Block blocks[] = {
 
 	{"$HOME/.local/bin/dwmblocks/cmus.sh",              1,		20},
 
-	{"$HOME/.local/bin/dwmblocks/network.sh",	        10,		0},
+	{"$HOME/.local/bin/dwmblocks/network.sh",	        1,		0},
 	
-	{"$HOME/.local/bin/dwmblocks/battery.sh",		5,		0},
+	{"$HOME/.local/bin/dwmblocks/battery.sh",		1,		0},
 
 	{"$HOME/.local/bin/dwmblocks/volume.sh",		1,		10},
 
 	{"$HOME/.local/bin/dwmblocks/time.sh",	        1,		0},
 
-	{"$HOME/.local/bin/dwmblocks/date.sh",	        60,		0},
+	{"$HOME/.local/bin/dwmblocks/date.sh",	        1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
