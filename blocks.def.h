be872bbdec6b85efa9e1e9cf360ace0039c3f3dc
UzayrQamarxyz <uzayrqamarxyz@tutanota.com>
2020-11-18 18:00:51 +0500

    updated time intervals

--- blocks.def.h
+++ blocks.def.h
@@ -1,17 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", "$HOME/.local/bin/dwmblocks/cmus.sh", 1, 20},
+    {"", "$HOME/.local/bin/dwmblocks/cmus.sh", 0, 20},
 
-    {"", "$HOME/.local/bin/dwmblocks/network.sh", 1, 0},
+    {"", "$HOME/.local/bin/dwmblocks/network.sh", 0, 0},
 
-    {"", "$HOME/.local/bin/dwmblocks/battery.sh", 1, 0},
+    {"", "$HOME/.local/bin/dwmblocks/battery.sh", 0, 0},
 
-    {"", "$HOME/.local/bin/dwmblocks/volume.sh", 1, 10},
+    {"", "$HOME/.local/bin/dwmblocks/volume.sh", 0, 10},
 
-    {"", "$HOME/.local/bin/dwmblocks/date.sh", 30, 0},
+    {"", "$HOME/.local/bin/dwmblocks/date.sh", 0, 0},
 
-    {"", "$HOME/.local/bin/dwmblocks/time.sh", 1, 0},
+    {"", "$HOME/.local/bin/dwmblocks/time.sh", 0, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
