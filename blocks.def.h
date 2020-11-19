1058ea49f775b01b17bd8f582a69b61410c0e9cb
UzayrQamarxyz <uzayrqamarxyz@tutanota.com>
2020-11-19 15:04:04 +0500

    cmus update interval

--- blocks.def.h
+++ blocks.def.h
@@ -1,7 +1,7 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
     /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {"", "$HOME/.local/bin/dwmblocks/cmus.sh", 0, 20},
+    {"", "$HOME/.local/bin/dwmblocks/cmus.sh", 1, 20},
 
     {"", "$HOME/.local/bin/dwmblocks/network.sh", 1, 0},
 
