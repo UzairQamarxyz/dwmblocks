03868e0baffa576869525fde48652a0a3641c70d
UzairQamarxyz <uzairqamarxyz@gmail.com>
2023-11-15 22:47:44 +0500

    Adding cmus
    Signed-off-by: UzairQamarxyz <uzairqamarxyz@gmail.com>

--- config.c
+++ config.c
@@ -4,7 +4,8 @@
 #include "util.h"
 
 Block blocks[] = {
-    {"sb-volume",     10,  10 },
+    {"sb-cmus",        1,  20 },
+    {"sb-volume",      1,  10 },
     {"sb-battery",     1,   0 },
     {"sb-network",     1,   0 },
     {"sb-date",        1,   0 },
